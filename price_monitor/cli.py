e7074eabd524e317bf83d4781c172996dcdbb8d9
ch0rt1k555 <vlad.zangiyev.97@mail.ru>
2026-09-14 22:55:52 +0300

    price_monitor/cli.py

--- price_monitor/cli.py
+++ price_monitor/cli.py
@@ -1,7 +1,33 @@
 import argparse
+import requests
+from bs4 import BeautifulSoup
+
+# def fetch_url(url):
+#     try:
+#         response = requests.get(url).text
+#         return response
+#     except Exception as e:
+#         print(f"Ошибка при запросе: {e}")
+def fetch_url(url):
+    try:
+        # 1. Маскируемся под настоящий браузер (Chrome на Windows)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7"
+        }
+        
+        response = requests.get(url, headers=headers)
+        
+        # 2. Принудительно устанавливаем правильную кодировку текста
+        response.encoding = 'utf-8' 
+        
+        return response.text
+    except Exception as e:
+        print(f"Ошибка при запросе: {e}")
+
+
 
 def main():
-    print("--- CLI START ---")
     parser = argparse.ArgumentParser(description="Price Monitor CLI Manager")
 
     parser.add_argument(
@@ -11,7 +37,17 @@ def main():
     subparsers = parser.add_subparsers(dest="command", required=True)
     parser_check = subparsers.add_parser("check", help="Check product price by URL")
 
+    parser_check.add_argument('url',  help='Ссылка на товар')
+
+    
     args = parser.parse_args()
+
+ 
+    if args.command == 'check':
+        url = fetch_url(args.url)
+        soup = BeautifulSoup(url, 'lxml')
+        print(soup)
+
     if not args.command:
         parser.print_help()
 
